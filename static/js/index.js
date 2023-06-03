eb0bf4732ee5cedc36023cbee57f8a2e1441ae9c
Sergio A. Gamarra <sergioagamarra@gmail.com>
2023-06-02 23:06:51 -0300

    Correcciones

--- static/js/index.js
+++ static/js/index.js
@@ -1,64 +1,247 @@
-const elementos = {
-    cantidadInput: document.getElementById("cantidad"),
-    categoriaSelect: document.getElementById("categoria"),
-    precioH3: document.getElementById("precio"),
-    resumenBtn: document.getElementById("btn-resumen"),
-    borrarBtn: document.getElementById("btn-borrar"),
-    divBtn: document.getElementById("botones"),
-    formNombre: document.getElementById("form_nombre"),
-    formApellido: document.getElementById("form_apellido"),
-    formCorreo: document.getElementById("form_correo")
-  };
-  
-  elementos.precioH3.style.display = "none";
-  
-  elementos.resumenBtn.addEventListener("click", () => {
-    const { cantidadInput, categoriaSelect, precioH3 } = elementos;
-    const cantidad = parseInt(cantidadInput.value);
-  
-    if (cantidad) {
-      const categoria = parseInt(categoriaSelect.value);
-      let precioTotal = cantidad * 200;
-  
-      switch (categoria) {
-        case 1:
-          precioTotal -= precioTotal * 0.8;
-          break;
-        case 2:
-          precioTotal -= precioTotal * 0.5;
-          break;
-        case 3:
-          precioTotal -= precioTotal * 0.15;
-          break;
-      }
-  
-      precioH3.textContent = `$ ${precioTotal}`;
-      precioH3.style.display = "block";
-    } else {
-      const formulario = document.getElementById("formulario");
-      const alertDiv = document.createElement("div");
-      alertDiv.className = "row justify-content-center";
-      alertDiv.innerHTML = `
+const elements = {
+    formAmount: document.getElementById("formAmount"),
+    formCategory: document.getElementById("formCategory"),
+    formPrice: document.getElementById("formPrice"),
+    formBtnSummary: document.getElementById("formBtnSummary"),
+    formBtnDelete: document.getElementById("formBtnDelete"),
+    formDivButtons: document.getElementById("formDivButtons"),
+    form: document.getElementById("form"),
+    formName: document.getElementById("formName"),
+    formLastName: document.getElementById("formLastName"),
+    formMail: document.getElementById("formMail"),
+    cardStudent: document.getElementById("cardStudent"),
+    cardTrainee: document.getElementById("cardTrainee"),
+    cardJunior: document.getElementById("cardJunior"),
+    modal: document.getElementById("modal"),
+    modalName: document.getElementById("modalName"),
+    modalLastName: document.getElementById("modalLastName"),
+    modalMail: document.getElementById("modalMail"),
+    modalAmount: document.getElementById("modalAmount"),
+    modalCategory: document.getElementById("modalCategory"),
+    modalTotal: document.getElementById("modalTotal"),
+    modalBtn: document.getElementById("modalBtn"),
+};
+
+elements.formPrice.style.display = "none";
+
+elements.formAmount.addEventListener("input", updatePrice);
+
+elements.formCategory.addEventListener("change", () => {
+    const category = parseInt(elements.formCategory.value);
+    updateCardSelected(category);
+    updatePrice();
+});
+
+elements.cardStudent.addEventListener("click", () => {
+    elements.formCategory.value = "2";
+    updateCardSelected(2);
+    updatePrice();
+});
+
+elements.cardTrainee.addEventListener("click", () => {
+    elements.formCategory.value = "3";
+    updateCardSelected(3);
+    updatePrice();
+});
+
+elements.cardJunior.addEventListener("click", () => {
+    elements.formCategory.value = "4";
+    updateCardSelected(4);
+    updatePrice();
+});
+
+elements.formBtnSummary.addEventListener("click", validateForm);
+
+elements.formBtnDelete.addEventListener("click", cleanForm);
+
+elements.modalBtn.addEventListener("click", function () {
+    let modal = bootstrap.Modal.getInstance(elements.modal);
+    modal.hide();
+    showAlert("Se ha completado la compra de tickects con éxito.", 2);
+});
+
+function showAlert(message, type) {
+    const form = elements.form;
+    const alertDiv = document.createElement("div");
+    let icon;
+    let alert;
+
+    switch (type) {
+      case 1:
+        icon = `<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>`;
+        alert = "danger";
+        break;
+      case 2:
+        icon = `<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>`;
+        alert = "success";
+        break;
+      default:
+        break;
+    }
+
+    alertDiv.className = "row justify-content-center";
+    alertDiv.innerHTML = `
         <div class="col-md-6">
-          <div class="alert alert-danger d-flex justify-content-start" role="alert">
-            ¡Por favor ingrese la cantidad de tickets que desea comprar!
+          <div class="alert alert-${alert} d-flex justify-content-start" role="alert">
+            ${icon}
+            ${message}
           </div>
         </div>
       `;
-      formulario.insertBefore(alertDiv, elementos.divBtn);
-      setTimeout(() => {
+    form.insertBefore(alertDiv, elements.formDivButtons);
+    
+    cleanForm();
+
+    setTimeout(() => {
         alertDiv.remove();
-      }, 3000);
+    }, 3000);
+
+    return 0;
+}
+
+function calculatePrice(amount, category) {
+    let totalPrice = amount * 200;
+
+    switch (category) {
+        case 2:
+            totalPrice -= totalPrice * 0.8;
+            break;
+
+        case 3:
+            totalPrice -= totalPrice * 0.5;
+            break;
+
+        case 4:
+            totalPrice -= totalPrice * 0.15;
+            break;
     }
-  });
-  
-  elementos.borrarBtn.addEventListener("click", () => {
-    const { formNombre, formApellido, formCorreo, cantidadInput, categoriaSelect, precioH3 } = elementos;
-  
-    formNombre.value = "";
-    formApellido.value = "";
-    formCorreo.value = "";
-    cantidadInput.value = "";
-    categoriaSelect.value = "1";
-    precioH3.style.display = "none";
-  });
+
+    return totalPrice;
+}
+
+function updatePrice() {
+    const { formAmount, formCategory, formPrice } = elements;
+    const amount = parseInt(formAmount.value);
+    const category = parseInt(formCategory.value);
+
+    if (!amount || amount < 1) {
+        formPrice.textContent = "";
+        return;
+    }
+
+    const totalPrice = calculatePrice(amount, category);
+
+    formPrice.textContent = `$ ${totalPrice}`;
+    formPrice.style.display = "block";
+}
+
+function updateCardSelected(category) {
+    const { cardStudent, cardTrainee, cardJunior } = elements;
+
+    cardStudent.classList.remove("card-estudiante");
+    cardTrainee.classList.remove("card-trainee");
+    cardJunior.classList.remove("card-junior");
+
+    switch (category) {
+        case 2:
+            cardStudent.classList.add("card-estudiante", "cursor-pointer");
+            break;
+
+        case 3:
+            cardTrainee.classList.add("card-trainee", "cursor-pointer");
+            break;
+
+        case 4:
+            cardJunior.classList.add("card-junior", "cursor-pointer");
+            break;
+    }
+}
+
+function validateForm() {
+    const { formName, formLastName, formMail, formAmount } = elements;
+
+    const name =  formName.value.trim();
+    const lastName = formLastName.value.trim();
+    const mail = formMail.value.trim();
+    const amount = parseInt(formAmount.value);
+
+    if (!name || !lastName || !mail || !amount) {
+        showAlert("Por favor, complete todos los campos del formulario.", 1);
+        return;
+    }
+    if (amount < 1) {
+        showAlert("Por favor, ingrese una cantidad de tickets válida.", 1);
+        return;
+    }
+
+    generateSummary();
+}
+
+function generateSummary() {
+    const {
+        formName,
+        formLastName,
+        formMail,
+        formAmount,
+        formCategory,
+        modalName,
+        modalLastName,
+        modalMail,
+        modalAmount,
+        modalCategory,
+        modalTotal,
+        modal,
+    } = elements;
+
+    const name =  formName.value.trim();
+    const lastName = formLastName.value.trim();
+    const mail = formMail.value.trim();
+    const amount = parseInt(formAmount.value);
+    const category = parseInt(formCategory.value);
+    const totalPrice = calculatePrice(amount, category);
+
+    modalName.textContent = name;
+    modalLastName.textContent = lastName;
+    modalMail.textContent = mail;
+    modalAmount.textContent = amount;
+    modalCategory.textContent = getCategoryText(category);
+    modalTotal.textContent = `$ ${totalPrice}`;
+
+    const modalBoostrap = new bootstrap.Modal(modal);
+    modalBoostrap.show();
+}
+
+function getCategoryText(category) {
+    switch (category) {
+        case 2:
+            return "Estudiante";
+        case 3:
+            return "Trainee";
+        case 4:
+            return "Junior";
+        default:
+            return "General";
+    }
+}
+
+function cleanForm() {
+    const {
+        formName,
+        formLastName,
+        formMail,
+        formAmount,
+        formCategory,
+        formPrice,
+    } = elements;
+
+    formName.value = "";
+    formLastName.value = "";
+    formMail.value = "";
+    formAmount.value = "";
+    formCategory.value = "1";
+    formPrice.style.display = "none";
+
+    elements.cardStudent.classList.remove("card-estudiante");
+    elements.cardTrainee.classList.remove("card-trainee");
+    elements.cardJunior.classList.remove("card-junior");
+}
